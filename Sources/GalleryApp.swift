d50773a9fbc8bc7c8ad54b8aba9f83149f77711a
Viktor Sukochev <sukochevvv@gmail.com>
2021-08-14 14:12:18 +0300

    Initial project

---
+++ Sources/GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryApp.swift
+//  Gallery
+//
+//  Created by Victor Sukochev on 14.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ Sources/UI/Main/MainView.swift
@@ -0,0 +1,16 @@
+//
+//  MainView.swift
+//  Gallery
+//
+//  Created by Victor Sukochev on 14.08.2021.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    
+    var body: some View {
+        Text("Main")
+    }
+    
+}
